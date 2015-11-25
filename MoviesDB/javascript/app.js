3642ce244dc69a55400cd5ad94d5f982a1e8d136
Sean Cahill <projects@nmm.ie>
2015-11-25 14:45:34 +0000

    first commit

--- MoviesDB/javascript/app.js
+++ MoviesDB/javascript/app.js
@@ -1,2 +1,31 @@
 // defining the app module of the project
-angular.module('moviesDBApp', ['movieDBControllers','movieDBServices']);
+angular.module('moviesDBApp', ['ngRoute','movieDBControllers','movieDBServices'])
+.constant("myMovieConfig", {
+       "moviesEndpoint" : "https://api.themoviedb.org/3/movie",
+       "apiKey": "35e16679c616a21b9ddebb66272c5902"
+   })
+.config(function($routeProvider) {     
+        $routeProvider
+          .when('/', {
+              templateUrl: 'templates/movies.html',
+              controller: 'MovieListController'
+          })
+          .when('/popular', {
+              templateUrl: 'templates/movies.html',
+              controller: 'MovieListController'
+          })
+          .when('/upcoming', {
+              templateUrl: 'templates/movies.html',
+            controller: 'MovieUpcomingController'
+          })
+          .when('/topRated', {
+              templateUrl: 'templates/movies.html',
+            controller: 'MovieTopRatedController'
+          })
+          .when("/nowPlaying", {
+            templateUrl: "templates/movies.html",
+            controller: "MovieNowPlayingController"
+        })
+         .otherwise({redirectTo: '/'});
+     });
+
